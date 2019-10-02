6bf5a03a559e233101f5d00f0c00b6a5008d3e41
Adhitya Ramadhanus <adhityaramadhanus@ITs-MacBook-Air-2.local>
2019-10-02 18:48:36 +0700

    change keyvalue service

--- keyvalue.go
+++ keyvalue.go
@@ -1,11 +1,17 @@
 package userland
 
 import (
+	"errors"
 	"time"
+)
+
+var (
+	ErrKeyNotFound = errors.New("Key not found")
 )
 
 type KeyValueService interface {
 	Set(key string, value []byte) error
 	SetEx(key string, value []byte, expirationInSeconds time.Duration) error
 	Get(key string) ([]byte, error)
+	Delete(key string) error
 }
--- storage/redis/keyvalue.go
+++ storage/redis/keyvalue.go
@@ -3,6 +3,8 @@ package redis
 import (
 	"time"
 
+	"github.com/AdhityaRamadhanus/userland"
+
 	"github.com/go-redis/redis"
 )
 
@@ -20,12 +22,25 @@ func NewKeyValueService(redisClient *redis.Client) *KeyValueService {
 
 //Get a cache in bytes from a key
 func (c KeyValueService) Get(key string) (result []byte, err error) {
-	return c.redisClient.Get(key).Bytes()
+	val, err := c.redisClient.Get(key).Bytes()
+	if err != nil {
+		if err == redis.Nil {
+			return nil, userland.ErrKeyNotFound
+		}
+		return nil, err
+	}
+
+	return val, nil
 }
 
 //Set cache in bytes with key without expiration
 func (c KeyValueService) Set(key string, value []byte) (err error) {
 	return c.redisClient.Set(key, string(value), 0).Err()
+}
+
+//Set cache in bytes with key without expiration
+func (c KeyValueService) Delete(key string) (err error) {
+	return c.redisClient.Del(key).Err()
 }
 
 //SetEx cache in bytes with key with expiration
