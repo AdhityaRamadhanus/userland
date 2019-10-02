15c4b3b622c1507d98d6f800ccfa167726690e0e
Adhitya Ramadhanus <adhityaramadhanus@ITs-MacBook-Air-2.local>
2019-10-02 18:47:12 +0700

    create new func in UserRepo, add BackupCodes in model user

--- user.go
+++ user.go
@@ -1,32 +1,39 @@
 package userland
 
 import (
+	"errors"
 	"time"
 )
 
 //User is domain entity
 type User struct {
-	ID         int
-	Email      string
-	Fullname   string
-	Phone      string
-	Location   string
-	Bio        string
-	WebURL     string
-	PictureURL string
-	Password   string
-	TFAEnabled bool
-	Verified   bool
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
+	ID          int
+	Email       string
+	Fullname    string
+	Phone       string
+	Location    string
+	Bio         string
+	WebURL      string
+	PictureURL  string
+	Password    string
+	TFAEnabled  bool
+	Verified    bool
+	BackupCodes []string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
+
+var (
+	ErrUserNotFound = errors.New("User not found")
+)
 
 //UserRepository provide an interface to get user entities
 type UserRepository interface {
 	Find(id int) (User, error)
 	FindByEmail(email string) (User, error)
-	FindByEmailAndPassword(email, password string) (User, error)
 	Insert(user User) error
 	Update(user User) error
+	// problematic func here
+	StoreBackupCodes(user User) error
 	Delete(id int) error
 }
